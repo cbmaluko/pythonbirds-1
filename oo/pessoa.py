e8f4fae9298a218638b51d9484e3a93ba9eb31d6
MATHIAS <sacmicrotech@hotmail.com>
2020-01-23 20:01:09 -0300

    Criado um atributo complexo

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,19 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome  = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Mathias', 40)
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    print(p.idade)
-    p.nome = 'teste'
-    print(p.nome)
+    filho1 = Pessoa(nome='Ana Laura')
+    filho2 = Pessoa(nome='Bernardo')
+    mathias = Pessoa(filho1, filho2, nome='Mathias')
+    print(Pessoa.cumprimentar(mathias))
+    print(mathias.nome)
+    print(mathias.idade)
+
+    for filho in mathias.filhos:
+        print(filho.nome)
